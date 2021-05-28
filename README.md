b76d49d88e54ba4b89e361b7b822914a93191b04
Jethro Larson <jethro.larson@tableau.com>
2021-05-28 13:11:06 -0700

    Refine challenge requirements

---
+++ README.md
@@ -0,0 +1,11 @@
+# Challenge: Shared Todo List
+
+Create an API for a TODO list. Open book, google, use any libraries you like.
+
+## Use cases
+* User can create a new todo list
+* User can add a list item to todo list
+* User can mark list item as done
+
+
+_This page left intentionally vague_
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,3 @@ app.get("/", (_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// Challenge: Shared Todo List
-// Create an API for a TODO list
-// Open book, google, any libraries you like
-// use cases:
-// user can create a new todo list
-// user can add a list item to todo list
-// user can mark list item as done
